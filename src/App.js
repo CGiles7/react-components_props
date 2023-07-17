19ba69f2ef08e782cd61e8b3ac7c2cced2b91be9
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-17 17:46:16 -0400

    add components with props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Holiday from "./Holiday";
 
-function App() {}
+function App() {
+    return (
+        <React.Fragment>
+            <Holiday name="Valentine's Day" day="14" month="February" />
+            <Holiday name="Christmas" day="25" month="December" />
+        </React.Fragment>
+    );
+}
 
 export default App;
